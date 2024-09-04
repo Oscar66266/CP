c00d4a72c673e2077c33fb0d117025f829de39c6
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2024-09-03 23:03:34 -0500

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 export async function before(m, { conn }) {
-   let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://github.com/Oscar66266/CP/blob/main/src/IMG-20240902-WA0629.jpg');
+   let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'IMG-20240902-WA0629.jpg');
 
   let nam = "【 ✯ Starlights Team - Oficial Chanel ✰ 】"
   
